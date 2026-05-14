9ce7705fbe6ff78e32cd643b1fc436a535d5baac
Yishay Cooper <yishaycooper@gmail.com>
2026-05-14 13:40:13 +0300

    tikTokLink

--- Creds/app/src/main/java/com/gobieyon/creds/Item.kt
+++ Creds/app/src/main/java/com/gobieyon/creds/Item.kt
@@ -4,6 +4,7 @@ data class Item(
     val author: String,
     val cat: String,
     val credLink: String = "",
+    val tikTokLink: String = "",
     val isSameUrl: Boolean = false,
     val link: String,
     val videoId: String,
